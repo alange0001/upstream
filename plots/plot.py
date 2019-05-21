7884d164f9cdb68dc19fc6290c86d34c20fe05c0
Adriano Lange <alange0001@gmail.com>
2019-05-20 21:25:34 -0400

    plots and experiments

--- plots/plot.py
+++ plots/plot.py
@@ -1,58 +1,34 @@
 
-project_dir = '/home/alange/workspace/dr/topics1/upstream'
+class config:
+	project_dir = '/home/alange/workspace/dr/topics1/upstream'
+	host_dir    = '/media/auto/alange-ms01-r/virtual/hostshare'
+	vm_prefix   = 'test-load'
+	vm_count    = 5
 
 import sys
-sys.path.append(project_dir)
-import util
 import collections
 
-import numpy
+import numpy as np
 from scipy.stats import logistic
 import matplotlib.pyplot as plt
 
-'''
-####### examples ########
-fig, ax = plt.subplots(1, 1)
+sys.path.append(config.project_dir)
+import util
+import guestmodel
 
-mean, var, skew, kurt = logistic.stats(moments='mvsk')
+def getVMFile(n):
+	#return '{}/profile/test-load{}/stats-1'.format(config.host_dir, i)
+	return '{}/plots/profile/test-load{}/stats-1'.format(config.project_dir, i)
 
-x = numpy.linspace(logistic.ppf(0.01),
-		logistic.ppf(0.99), 100)
+#############################################################
+# Usage, Steal, and Demand
 
-ax.plot(x, logistic.pdf(x),
-		'r-', lw=5, alpha=0.6, label='logistic pdf')
-
-rv = logistic()
-ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
-
-
-vals = logistic.ppf([0.001, 0.5, 0.999])
-numpy.allclose([0.001, 0.5, 0.999], logistic.cdf(vals))
-
-r = logistic.rvs(size=1000)
-
-ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
-ax.legend(loc='best', frameon=False)
-#'''
-
-'''
-####### tests ########
-loc=5
-scale=1
-x = numpy.linspace(0, 10, 50)
-ax.plot(x, logistic.sf(x, loc=loc, scale=scale), 'k-', lw=2, label='cdf')
-#'''
-
-
-vm_count = 5
-
-fig, axs = plt.subplots(vm_count, 1)
+fig, axs = plt.subplots(config.vm_count, 1)
 fig.set_figheight(7)
 
-for i in range(1,vm_count+1):
-	name    = 'test-load{}'.format(i)
-	#vm_file = '/media/auto/alange-ms01-r/virtual/hostshare/profile/test-load{}/stats-1'.format(i)
-	vm_file = '/media/auto/alange-ms01-r/virtual/hostshare/usr_global/src/upstream/plots/profile-data2/test-load{}/stats-1'.format(i)
+for i in range(1,config.vm_count+1):
+	name    = '{}{}'.format(config.vm_prefix, i)
+	vm_file = getVMFile(i)
 
 	log = util.WatchLog(vm_file, 'json')
 	lines = log.read()
@@ -63,10 +39,65 @@ for i in range(1,vm_count+1):
 	steal = [ float(l['vm']['vcpu'][0]['steal']) for l in lines ]
 	demand = [ float(l['vm']['vcpu'][0]['usage']) + float(l['vm']['vcpu'][0]['steal']) for l in lines ]
 
-	axs[i-1].set_ylim(ymin=-5, ymax=110)
+	if i == 1:
+		vm1_lines = lines
+		vm1_x = x
+		vm1_usage = usage
+
+	axs[i-1].set_ylim(bottom=-5, top=110)
 	axs[i-1].plot(x, usage,  lw=1)
 	axs[i-1].plot(x, steal,  lw=1)
 	axs[i-1].plot(x, demand, lw=1)
 
 fig.legend(loc='lower center', labels=['usage', 'steal', 'VM\'s demand'], ncol=3, frameon=True)
 plt.show()
+
+#############################################################
+# Model
+fig, ax = plt.subplots(1, 1)
+fig.set_figheight(4)
+
+m = guestmodel.Model('', 300)
+vm1_otherdemand = []
+vm1_predict     = []
+for l in vm1_lines:
+	m_line = m.feed(l)
+	vm1_otherdemand.append(m_line['other_demand'] * 100.)
+	vm1_predict.append(
+		100. * m.predict(
+			m_line['cpus'],
+			l['vm']['vcpu_count'],
+			m_line['other_demand']))
+
+#ax.set_ylim(bottom=-5, top=110)
+ax.plot(vm1_x, vm1_usage,       lw=1)
+ax.plot(vm1_x, vm1_otherdemand, lw=1)
+ax.plot(vm1_x, vm1_predict,     lw=1, color='red',)
+
+fig.legend(loc='lower center', labels=['usage', 'others\' demand', 'model'], ncol=3, frameon=True)
+plt.show()
+
+#############################################################
+fig, ax = plt.subplots(1, 1)
+fig.set_figheight(4)
+
+ax.plot(vm1_otherdemand, vm1_usage,   'o', lw=1)
+ax.plot(vm1_otherdemand, vm1_predict, 'o', lw=1, color='red')
+
+plt.show()
+
+
+'''
+
+loc=50
+scale=5
+fig, ax = plt.subplots(1, 1)
+ax.set_ylim(bottom=-0.05, top=1.05)
+
+x = np.linspace(0, 100, 100)
+
+ax.plot(x, .6 + .4 * logistic.sf(x, loc=loc, scale=scale), lw=1)
+
+plt.show()
+
+#'''
